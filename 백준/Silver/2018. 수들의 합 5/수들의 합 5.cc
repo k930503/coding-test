bd125017451a7a6be6737f520d97c3ef040c1a04
Youngmin <k930503@gmail.com>
2024-03-18 23:28:02 +0900

    [Silver V] Title: 수들의 합 5, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -1,41 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int calculateSequenceSum(int start, int n) {
-    return n * (2 * start + n - 1) / 2;
-}
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n,pos1=1,pos2=1,sum=1,cnt=1;
+	cin >> n;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+	while (pos2 != n)
+	{
+		if (sum == n)
+		{
+			cnt++, pos2++;
+			sum += pos2;
+            sum -= pos1;
+            pos1++;
+		}
+		else if (sum > n)
+		{
+			sum -= pos1;
+			pos1++;
 
-    int N;
-    int start = 1;
-    int end = 1;
-    int cnt = 0;
+		}
+		else
+		{
+			
+			pos2++;
+			sum += pos2;
+		}
+	}
 
-    cin >> N;
-
-    while (start <= N) {
-        int temp = calculateSequenceSum(start, end - start + 1);
-        if (temp == N) {
-            cnt++;
-            if (end == N) {
-                break;
-            }
-            start++;
-            end++;
-        } else if (temp > N) {
-            start++;
-        } else {
-            if (end == N) {
-                break;
-            }
-            end++;
-        }
-    }
-    cout << cnt << "\n";
-    return 0;
+	printf("%d", cnt);
+	return 0;
 }
