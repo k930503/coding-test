5d14dce8a1250edab1236de5c0780f3b8db08fb5
Youngmin <k930503@gmail.com>
2024-03-22 21:59:38 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2288 ms, Memory: 43588 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+typedef pair<int, int> Node;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, L;
+    cin >> N >> L;
+    deque<Node> deque;
+
+    for (int i = 0; i < N; i++) {
+        int now;
+        cin >> now;
+        // 값이 들어올 때마다 정렬하지 않고
+        // 현재 수보다 큰 값을 덱에서 제거하여 시간 복잡도를 줄임
+        while (deque.size() && deque.back().first > now) {
+            deque.pop_back();
+        }
+        deque.push_back(Node(now, i));
+        // 범위에서 벗어난 값은 덱에서 제거
+        if (deque.front().second <= i - L) {
+            deque.pop_front();
+        }
+        cout << deque.front().first << ' ';
+    }
+    return 0;
+}
