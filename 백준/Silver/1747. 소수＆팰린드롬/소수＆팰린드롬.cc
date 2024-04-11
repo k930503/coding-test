2aabd2373189eabcca1049504967004c81d88ef4
Youngmin <k930503@gmail.com>
2024-04-11 20:37:46 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int val) {
+    if (val == 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= val; i++) {
+        if (val % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPalindrom(int val) {
+    string str = to_string(val);
+    for (int i = 0; i < ceil((int)str.length() / 2.0); i++) {
+        if (str[i] != str[str.length() - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int n;
+    cin >> n;
+
+    while (!isPrime(n) || !isPalindrom(n)) {
+        n++;
+    }
+    cout << n;
+    return 0;
+}
