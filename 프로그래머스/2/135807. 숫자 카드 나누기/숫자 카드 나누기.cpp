d1df95df597db3ea8e03667a7022ea3dc113f900
Youngmin <k930503@gmail.com>
2024-07-26 20:06:04 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 10.57 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int gcd(int a, int b) {
+    int temp;
+    if (a < b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    int mod = a % b;
+    if (mod == 0) {
+        return b;
+    }
+    return gcd(b, mod);
+}
+
+int findMaxDivisor(vector<int>& arrayA, vector<int>& arrayB) {
+    int answer = 0;
+    int gcdVal = arrayA[0];
+    for (int i = 1; i < arrayA.size(); i++) {
+        gcdVal = gcd(gcdVal, arrayA[i]);
+    }
+    vector<int> divisors;
+    for (int i = 1; i <= gcdVal; i++) {
+        if (gcdVal % i == 0) {
+            divisors.push_back(i);
+        }
+    }
+    
+    for (int i = divisors.size() - 1; i >= 0; i--) {
+        bool founded = true;
+        for (int j = 0; j < arrayB.size(); j++) {
+            if (arrayB[j] % divisors[i] == 0) {
+                founded = false;
+                break;
+            }
+        }
+        if (founded) {
+            return divisors[i];
+        }
+    }
+    return answer;
+}
+
+int solution(vector<int> arrayA, vector<int> arrayB) {
+    int a = findMaxDivisor(arrayA, arrayB);
+    int b = findMaxDivisor(arrayB, arrayA);
+    if (a < b) {
+        a = b;
+    }
+    return a;
+}
